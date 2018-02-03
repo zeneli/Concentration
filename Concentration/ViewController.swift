f3926ae46d514bd2e46f782a80a62599d1fd8015
zeneli <lidozeneli@gmail.com>
2018-02-02 22:38:12 -0500

    touchCard: Stub touchCard UIButton IBAction.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        print("agh! a ghost!")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
