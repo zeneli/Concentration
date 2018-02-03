10f732a5f1241fa24b0efcf4738d4c595c1faa79
zeneli <lidozeneli@gmail.com>
2018-02-02 22:55:27 -0500

    flipCard: Add flipCard helper to implement touchCard.
    When a user touches the card, flipCard switches between two states.
    One state shows the image (currently a ghost emoji on white background).
    The other state shows the back of the card (currently orange background with no text).

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -11,7 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func touchCard(_ sender: UIButton) {
-        print("agh! a ghost!")
+        flipCard(withEmoji: "👻", on: sender)
     }
     
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
