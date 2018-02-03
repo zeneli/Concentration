00ed6cb88fb663503a4a1dce89032219eb477892
zeneli <lidozeneli@gmail.com>
2018-02-03 00:25:33 -0500

    touchSecondCard: Add a second card.
    Create a second card (with emoji pumpkin).
    Add IBAction method touchSecondCard to toggle the card.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -13,8 +13,14 @@ class ViewController: UIViewController {
     @IBAction func touchCard(_ sender: UIButton) {
         flipCard(withEmoji: "👻", on: sender)
     }
+
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard(withEmoji: \(emoji))")
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
