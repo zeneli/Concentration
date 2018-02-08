7e532fa98d798a96bbeee9d8c4417dd46ce7bf4c
zeneli <lidozeneli@gmail.com>
2018-02-08 18:36:47 -0500

    model: Initialize Concentration and Card model.

---
+++ Concentration/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Amarildo Zeneli on 2/8/18.
+//  Copyright © 2018 Amarildo Zeneli. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Amarildo Zeneli on 2/8/18.
+//  Copyright © 2018 Amarildo Zeneli. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards = Array<Card>()
+    
+    func chooseCard(at index: Int) {
+        
+    }
+}
