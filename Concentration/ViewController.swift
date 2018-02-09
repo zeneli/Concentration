a442eb7a951c66e19e1e81775b9f96324565bcd5
zeneli <lidozeneli@gmail.com>
2018-02-09 10:48:32 -0500

    touchCard: Refactor to update view from model.
    In Concentration model, implement chooseCard function to choose a chard
    from an index.
    In ViewController, implement updateFromModel function to loop through
    all card models and update the corresponding button view objects.
    Stub function emoji to choose an emoji for a card object.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var game = Concentration(numberOfPairsOfCards: 4)
+    var game = Concentration(numberOfPairsOfCards: 2)
     
     var flipCount = 0 {
         didSet {
@@ -22,27 +22,34 @@ class ViewController: UIViewController {
     
     @IBOutlet var cardButtons: [UIButton]!
     
-    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
-    
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
-            print("cardNumber is \(cardNumber)")
-            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
         } else {
             print("chosen card was not in cardButtons")
         }
     }
 
-    
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        print("flipCard(withEmoji: \(emoji))")
-        if button.currentTitle == emoji {
-            button.setTitle("", for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+    func updateViewFromModel() {
+        // Go through all the cards, look at the game, and set it up appropriately.
+        for index in cardButtons.indices {
+            let button = cardButtons[index]  // a view object
+            let card = game.cards[index]    // a model object
+            if card.isFaceUp {  // update the view according to the model object
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+            }
         }
+    }
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+
+    func emoji(for card: Card) -> String {
+        return "?"
     }
 }
